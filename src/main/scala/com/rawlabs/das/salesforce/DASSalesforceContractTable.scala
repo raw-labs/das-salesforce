d1a3efffa2b999a11bad63489c99dcaf1a135062
Miguel Branco <miguel@raw-labs.com>
2024-09-10 13:23:00 +0200

    Add remaining tables + CI (#1)

---
+++ src/main/scala/com/rawlabs/das/salesforce/DASSalesforceContractTable.scala
@@ -0,0 +1,321 @@
+/*
+ * Copyright 2024 RAW Labs S.A.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.txt.
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0, included in the file
+ * licenses/APL.txt.
+ */
+
+package com.rawlabs.das.salesforce
+
+import com.rawlabs.protocol.das.{ColumnDefinition, TableDefinition, TableId}
+import com.rawlabs.protocol.raw.{BoolType, IntType, RecordType, StringType, TimestampType, Type}
+
+class DASSalesforceContractTable(connector: DASSalesforceConnector)
+    extends DASSalesforceTable(connector, "salesforce_contract", "Contract") {
+
+  // TODO (msb): Add helper methods to create the table definition
+  // TODO (msb): Add dynamic columns based on the Salesforce schema
+  override def tableDefinition: TableDefinition = {
+    TableDefinition
+      .newBuilder()
+      .setTableId(TableId.newBuilder().setName(tableName))
+      .setDescription(
+        "Represents a contract (a business agreement) associated with an Account."
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("id")
+          .setDescription("Unique identifier of the contract in Salesforce.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("account_id")
+          .setDescription("ID of the Account associated with this contract.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("contract_number")
+          .setDescription("Number of the contract.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("contract_term")
+          .setDescription("Number of months that the contract is valid.")
+          .setType(Type.newBuilder().setInt(IntType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("end_date")
+          .setDescription(
+            "Calculated end date of the contract. This value is calculated by adding the ContractTerm to the start_date."
+          )
+          .setType(
+            Type.newBuilder().setTimestamp(TimestampType.newBuilder().setTriable(false).setNullable(true)).build()
+          )
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("owner_id")
+          .setDescription("ID of the user who owns the contract.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("start_date")
+          .setDescription("Start date for this contract.")
+          .setType(
+            Type.newBuilder().setTimestamp(TimestampType.newBuilder().setTriable(false).setNullable(true)).build()
+          )
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("status")
+          .setDescription(
+            "The picklist of values that indicate order status. A contract can be Draft, InApproval, or Activated."
+          )
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("activated_by_id")
+          .setDescription("ID of the User who activated this contract.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("activated_date")
+          .setDescription("Date and time when this contract was activated.")
+          .setType(
+            Type.newBuilder().setTimestamp(TimestampType.newBuilder().setTriable(false).setNullable(true)).build()
+          )
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("billing_address")
+          .setDescription("Billing address of the account.")
+          .setType(Type.newBuilder().setRecord(RecordType.newBuilder()).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("company_signed_date")
+          .setDescription("Date on which the contract was signed by organization.")
+          .setType(
+            Type.newBuilder().setTimestamp(TimestampType.newBuilder().setTriable(false).setNullable(true)).build()
+          )
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("company_signed_id")
+          .setDescription("ID of the User who signed the contract.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("created_by_id")
+          .setDescription("Id of the user who created the contract record.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("created_date")
+          .setDescription("Date and time when contract record was created.")
+          .setType(
+            Type.newBuilder().setTimestamp(TimestampType.newBuilder().setTriable(false).setNullable(true)).build()
+          )
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("customer_signed_date")
+          .setDescription("Date on which the customer signed the contract.")
+          .setType(
+            Type.newBuilder().setTimestamp(TimestampType.newBuilder().setTriable(false).setNullable(true)).build()
+          )
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("customer_signed_id")
+          .setDescription("ID of the Contact who signed this contract.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("customer_signed_title")
+          .setDescription("Title of the contact who signed the contract.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("description")
+          .setDescription("Statement describing the contract.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("is_deleted")
+          .setDescription("Indicates whether the object has been moved to the Recycle Bin (true) or not (false).")
+          .setType(Type.newBuilder().setBool(BoolType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("last_activity_date")
+          .setDescription(
+            "Value is one of the following, whichever is the most recent. a) Due date of the most recent event logged against the record. b) Due date of the most recently closed task associated with the record."
+          )
+          .setType(
+            Type.newBuilder().setTimestamp(TimestampType.newBuilder().setTriable(false).setNullable(true)).build()
+          )
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("last_approved_date")
+          .setDescription("Last date the contract was approved.")
+          .setType(
+            Type.newBuilder().setTimestamp(TimestampType.newBuilder().setTriable(false).setNullable(true)).build()
+          )
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("last_modified_by_id")
+          .setDescription("The id of user who most recently changed the contract record.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("last_modified_date")
+          .setDescription("The date and time of the last change to contract record.")
+          .setType(
+            Type.newBuilder().setTimestamp(TimestampType.newBuilder().setTriable(false).setNullable(true)).build()
+          )
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("last_referenced_date")
+          .setDescription(
+            "The timestamp when the current user last accessed this record, a record related to this record, or a list view."
+          )
+          .setType(
+            Type.newBuilder().setTimestamp(TimestampType.newBuilder().setTriable(false).setNullable(true)).build()
+          )
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("last_viewed_date")
+          .setDescription(
+            "The timestamp when the current user last viewed this record or list view. If this value is null, the user might have only accessed this record or list view (last_referenced_date) but not viewed it."
+          )
+          .setType(
+            Type.newBuilder().setTimestamp(TimestampType.newBuilder().setTriable(false).setNullable(true)).build()
+          )
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("owner_expiration_notice")
+          .setDescription(
+            "Number of days ahead of the contract end date (15, 30, 45, 60, 90, and 120). Used to notify the owner in advance that the contract is ending."
+          )
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("pricebook_2_id")
+          .setDescription("ID of the pricebook, if any, associated with this contract.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("special_terms")
+          .setDescription("Special terms that apply to the contract.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("system_modstamp")
+          .setDescription("The date and time when contract was last modified by a user or by an automated process.")
+          .setType(
+            Type.newBuilder().setTimestamp(TimestampType.newBuilder().setTriable(false).setNullable(true)).build()
+          )
+          .build()
+      )
+      .setStartupCost(1000)
+      .build()
+  }
+
+  override protected val fieldsCannotBeUpdated: Seq[String] = Seq(
+    "id",
+    "billing_address",
+    "shipping_address",
+    "is_deleted",
+    "created_by_id",
+    "created_date",
+    "last_modified_by_id",
+    "last_modified_date"
+  )
+
+}
---
+++ src/main/scala/com/rawlabs/das/salesforce/DASSalesforcePermissionSetTable.scala
@@ -0,0 +1,189 @@
+/*
+ * Copyright 2024 RAW Labs S.A.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.txt.
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0, included in the file
+ * licenses/APL.txt.
+ */
+
+package com.rawlabs.das.salesforce
+
+import com.rawlabs.protocol.das.{ColumnDefinition, TableDefinition, TableId}
+import com.rawlabs.protocol.raw.{BoolType, StringType, TimestampType, Type}
+
+class DASSalesforcePermissionSetTable(connector: DASSalesforceConnector)
+    extends DASSalesforceTable(connector, "salesforce_permission_set", "PermissionSet") {
+
+  // TODO (msb): Add helper methods to create the table definition
+  // TODO (msb): Add dynamic columns based on the Salesforce schema
+  override def tableDefinition: TableDefinition = {
+    TableDefinition
+      .newBuilder()
+      .setTableId(TableId.newBuilder().setName(tableName))
+      .setDescription(
+        "Represents a set of permissions that's used to grant more access to one or more users without changing their profile or reassigning profiles."
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("id")
+          .setDescription("The unique id of the permission set.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("name")
+          .setDescription("The permission set unique name in the API.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("description")
+          .setDescription("The description of the permission set.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("is_custom")
+          .setDescription(
+            "If true, the permission set is custom (created by an admin); if false, the permission set is standard and related to a specific permission set license."
+          )
+          .setType(Type.newBuilder().setBool(BoolType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("created_date")
+          .setDescription("The Created Date.")
+          .setType(
+            Type.newBuilder().setTimestamp(TimestampType.newBuilder().setTriable(false).setNullable(true)).build()
+          )
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("created_by_id")
+          .setDescription("The contact id of the user who created this permission set.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("has_activation_required")
+          .setDescription(
+            "Indicates whether the permission set requires an associated active session (true) or not (false)."
+          )
+          .setType(Type.newBuilder().setBool(BoolType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("is_owned_by_profile")
+          .setDescription("If true, the permission set is owned by a profile.")
+          .setType(Type.newBuilder().setBool(BoolType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("label")
+          .setDescription("The permission set label, which corresponds to Label in the user interface.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("last_modified_by_id")
+          .setDescription("The Last Modified By ID.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("last_modified_date")
+          .setDescription("The Last Modified Date.")
+          .setType(
+            Type.newBuilder().setTimestamp(TimestampType.newBuilder().setTriable(false).setNullable(true)).build()
+          )
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("license_id")
+          .setDescription(
+            "The ID of either the related PermissionSetLicense or UserLicense associated with this permission set."
+          )
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("namespace_prefix")
+          .setDescription(
+            "The namespace prefix for a permission set that's been installed as part of a managed package. If the permission set isn't packaged or is part of an unmanaged package, this value is empty."
+          )
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("permission_set_group_id")
+          .setDescription(
+            "If the permission set is owned by a permission set group, this field returns the ID of the permission set group."
+          )
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("profile_id")
+          .setDescription("If the permission set is owned by a profile, this field contains the ID of the Profile.")
+          .setType(Type.newBuilder().setString(StringType.newBuilder().setTriable(false).setNullable(true)).build())
+          .build()
+      )
+      .addColumns(
+        ColumnDefinition
+          .newBuilder()
+          .setName("system_modstamp")
+          .setDescription("The date and time when order record was last modified by a user or by an automated process.")
+          .setType(
+            Type.newBuilder().setTimestamp(TimestampType.newBuilder().setTriable(false).setNullable(true)).build()
+          )
+          .build()
+      )
+      .setStartupCost(1000)
+      .build()
+  }
+
+  override protected val fieldsCannotBeUpdated: Seq[String] = Seq(
+    "id",
+    "billing_address",
+    "shipping_address",
+    "is_deleted",
+    "created_by_id",
+    "created_date",
+    "last_modified_by_id",
+    "last_modified_date"
+  )
+
+}
